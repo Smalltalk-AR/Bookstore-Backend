788a15b91109ecf7e98d17392301b397c107485c
Julian Maestri <j.maestri@mercapsoftware.com>
2018-10-18 21:09:25 -0300

    Changes in docker build

---
+++ docker/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+/opt/pharo/pharo \
+	/opt/bookstore/Pharo.image \
+	start-book-store \
+	--port=44444 \
+	--bind-address=0.0.0.0 \
+	--db-database="${DATABASE_NAME}" \
+	--db-host="${DATABASE_HOSTNAME}" \
+	--db-password="${DATABASE_PASSWORD}" \
+	--db-port="${DATABASE_PORT:-3306}" \
+	--db-username="${DATABASE_USERNAME}"
